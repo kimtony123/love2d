db955313932804c901dd89e77864972aaef773cb
kimtony123 <kimtony571@gmail.com>
2024-05-16 16:38:41 +0300

    first commit

---
+++ conf.lua
@@ -0,0 +1,11 @@
+function love.conf(t)
+    t.identity = "data/saves"
+    t.version = "11.5"
+    t.console = true
+    t.externalstorage  = true
+    t.gammacorrect = true
+    t.audio.mic = true
+    t.window.title = "cool ball game"
+    t.window.icon = "icon/firefox.png"
+     
+end
